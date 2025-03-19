5eedbcd18c8fab6d779c5b8aba3c3a4c0161830f
ishananjana <hmanjana2001@gmail.com>
2025-03-20 02:09:19 +0530

    create user form page and upgrade the some of parts

---
+++ src/UserForm.js
@@ -0,0 +1,35 @@
+import { Grid2, Typography } from "@mui/material";
+const UserForm = props => {
+
+    return(
+        <Grid2
+            container
+            spacing={2}
+            sx={{
+                backgroundColor: '#ffffff',
+                marginBottom: '30px',
+                display: 'block'
+            }}
+        >
+            <Grid2 item xs={12}>
+                <Typography component={'h1'} sx={{ color: "000000"}}>User Form</Typography>
+            </Grid2>
+
+            <Grid2>
+                <Typography
+                    component={'label'}
+                    htmlFor = "id"
+                    sx={{
+                        color: "0000"
+                    }}
+                >
+                    ID
+                </Typography>
+            </Grid2>
+        </Grid2>
+
+
+    );
+}
+
+export default UserForm;
